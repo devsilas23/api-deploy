25d3112d1bb3dc1a5162313853d91ccd9f55c4a0
Silas Viana dos Santos (Ind - CORP) <00247029@grupopetropolis.com.br>
2020-10-06 18:35:13 -0300

    alteracao porta

--- app.js
+++ app.js
@@ -30,8 +30,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(process.env.PORT || '3000')
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   next(createError(404));
 });
 
@@ -46,4 +48,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app; */
