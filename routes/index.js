640e5617b0c212c6578f50be6480539e9565168d
Silas Viana dos Santos (Ind - CORP) <00247029@grupopetropolis.com.br>
2020-10-06 18:47:45 -0300

    alteracoes app

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const CultoController = require('../controllers/CultoController')
 
 
 // Rotas referente aos cultos...
+routes.get('/',CultoController.lista);
 routes.get('/cultos',CultoController.lista);
 routes.get('/cultos/ref/:id',CultoController.listaRef)
 routes.post('/cultos',CultoController.novo);
